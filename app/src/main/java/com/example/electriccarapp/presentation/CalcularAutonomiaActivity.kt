6a9ef1f32bfd65030751e2013c248a3be901c9f9
Daniel <dan_hasegawa@hotmail.com>
2023-03-26 18:56:41 -0300

    Fundamentos de Desenvolvimento Mobile nativo para Android

---
+++ app/src/main/java/com/example/electriccarapp/presentation/CalcularAutonomiaActivity.kt
@@ -0,0 +1,15 @@
+package com.example.electriccarapp.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.electriccarapp.R
+
+class CalcularAutonomiaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calcular_autonomia)
+    }
+
+
+}
--- app/src/main/java/com/example/electriccarapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/electriccarapp/presentation/MainActivity.kt
@@ -1,11 +1,12 @@
-package com.example.electriccarapp
+package com.example.electriccarapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.electriccarapp.R
 
 class MainActivity : AppCompatActivity() {
     lateinit var preco: EditText
@@ -20,20 +21,21 @@ class MainActivity : AppCompatActivity() {
         setupListeners()
     }
 
-    fun setupView() {
-        preco = findViewById(R.id.et_preco_kwh)
-        kmPercorrido = findViewById(R.id.et_km_percorrido)
+    private fun setupView() {
+//        preco = findViewById(R.id.et_preco_kwh)
+//        kmPercorrido = findViewById(R.id.et_km_percorrido)
         btnCalcular = findViewById(R.id.btn_calcular)
-        resultado = findViewById(R.id.tv_resultado)
+//        resultado = findViewById(R.id.tv_resultado)
     }
 
-    fun setupListeners() {
+    private fun setupListeners() {
         btnCalcular.setOnClickListener {
-            calcular()
+            //calcular()
+            startActivity(Intent(this, CalcularAutonomiaActivity::class.java))
         }
     }
 
-    fun calcular() {
+    private fun calcular() {
         val preco = preco.text.toString().toFloat()
         val km = kmPercorrido.text.toString().toFloat()
         val result = preco / km
